64834638918c1e3cc4ed3dd0ad1ccc1db63416c6
sbmraj03@gmail.com <sbmraj03@gmail.com>
2023-10-21 20:58:18 +0530

    Ep2->Igniting Our App

--- App.js
+++ App.js
@@ -1,30 +1,5 @@
-// creating nested html structure inside react
-/**
- * react elem at the end of the day is obj and this obj becomes html that the browser understand
- * react.createElement creates and obj and while rendering into the dom it converts obj into the html
- * 
- *  if 2 or more siblings are there then the 3rd arg will be an array aka array of children
- *  but what if no. of child incr, code becomes messy => t/f JSX came into exist
- * 
- * diff btw lib and framework-> react is a lib b/c it can work even in a smaller portion of our code like header or footer 
- * react is just a some piece of JS code
- */
-
-
-/**
- *  <div id="parent">
- *      <div id="child1">
- *          <h1>I'm h1 tag</h1>
- *          <h2>I'm h2 tag</h2>
- *      </div>
- * 
- *      <div id="child2">
- *          <h1>I'm h1 tag</h1>
- *          <h2>I'm h2 tag</h2>
- *      </div>
- *  </div>
- */
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent= React.createElement("div",{id: "parent"},[
     React.createElement("div",{id:"child1"},[
@@ -37,7 +12,59 @@ const parent= React.createElement("div",{id: "parent"},[
     ]),
 ]);
 
-console.log(parent);
 
 const root= ReactDOM.createRoot(document.getElementById("root")); 
 root.render(parent);
+
+
+
+/**
+ * 1. npm init (test command: jest, keyword: namaste react,akshay sain,S.R)
+ *    we will get package.json a/f s1 (package.json is a configuration for npm)
+ *      npm needs package.json->  to help identify the project and handle dependencies.
+ * Bundlres->   a development tool that combines many JavaScript code files into a single one that is production-ready loadable in the browser
+ * webpack,vite,parcel these all are bundlers
+ * 
+ * 2. npm install -D parcel (-D means i want parcel as a dev dependencies)
+ *  dev dependencies-> req in developmet phase that is when we are developing our app
+ *  normal dep-> used in production also 
+ *  
+ * In package.json under devdependencies we will find ~ and ^ sign(tidle and caret) 
+ * ~: it install the "MAJOR" version of parcel release
+ * ^: it automatically upgrade parcel to the new "MINOR" version of parcel release
+ * no sign: no upgrade
+ * t/f it is safe to use ^ because if major version is installed then alot of code will break
+ * 
+ * 
+ * a/f s2 we wil get node modules && package-lock.json (it keeps the track of the exact version of teh depedencies that is being installed)
+ * node modules-> it has all the packages inside it that we will use (our proj need parcel and parcel needs its own dependies and its dependies also depended on the other dependendcies "THESE ALL ARE KNOWN AS TRANSITIVE DEPENDENCIES")
+ * every dependencies have its own package.json and many other things thats why node modules is huge
+ *
+ * put node modules in gitignore as we dont want it to go into production b/c it can be automatically recreated using package.json and package-lock.json just type npm install it will reinstall the node modules if it gets deleted
+ * package.json and package-lock.json have the version of the dependencies t/f these are needed in the production
+ * 
+ * 3. npm parcel index.html (app gets ignited)
+ * npx parcel package_name means to execute a package while npm is just  
+ * when we do npx parcel goes to the source index.html and build a dev built for our app and host that dev built to local host 1234 if that is busy then it host the built to a new location
+ *  
+ * cdn link is not good to use -> it is a costly operation as we dont want to get our react from some other network call as we can already have it inside the node modules and the second thing is if some other version of react comes then we always have to change the cdn link to the new version 
+ * t/f we can install react as a package inside our node modules 
+ * 
+ * 4. npm install react (react comes in the ndoe modules and the package.json and package-lock.json)
+ * 5. npm i react-dom (i is short form of install)
+ * everytime we want to see our development we have to ignite our app usign npx parcel index.html
+ * 
+ * 6. npx parcel index.html (creates .parcel-cache and dist for faster build)
+ * use import from "react"; means react is coming from the node modules earlier it was coming from the cdn link
+ * parcel beautifies the uix of the error
+ * 
+ * import is not a normal js file t/f to use it we have to write type="module" in the script...it tells the browser tha it is of type module and not the normal js file
+ * 
+ * npx parcel build index.html -> this is a production build(but it will give error...remove main from package.json...main is actually entry pt. of the parcel which is conflicting with the index.html)
+ * 
+ * dist and parcel-cache can be regenerated t/f these have to be pushed into the .gitignore
+ * 
+ * BrowserList -> gives us permit to decide on which browsers version we want our code to run 
+ * 
+ * 
+ */
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Namaste React 🚀
+# Namaste React 🚀    
+
+# Parcel is a beast
+- creates a Dev Build
+- creates a Local Server
+- it is doing HMR (hot module replacement) -> refreshing our page
+- how parcel doing all above stuffs-> it uses a file watching algorithm (written in c++)
+- it does caching (faster build after the first build because it takes some xtra space (parcel_cache) )
+- Image optimization
+- does minification of our files also in the production build
+- Bundling
+- Compress our file(remove all the white spaces from our file and shift to the production)
+- uses consistent hashing 
+- code splitting
+- differential bundling (to support in the older browser aw)
+- Diagnostic
+- Error Handling
+- host our app in HTTPs aw
+- uses Tree Shaking Algorithm( removes unused code from the file)
+- creates Different bundles for dev and production build
